125bd790832de98dd9760338a68b696437ab62e7
tobe <tobeg3oogle@gmail.com>
2018-11-28 14:52:59 +0800

    Update test script to access files in docker container

--- examples/read_json_dataframe.py
+++ examples/read_json_dataframe.py
@@ -4,5 +4,5 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("official_kmeans").getOrCreate()
 
-df = spark.read.json("file:///Users/tobe/code/spark_examples/dataframe_examples/people.json")
+df = spark.read.json("file:///examples/people.json")
 df.show()
